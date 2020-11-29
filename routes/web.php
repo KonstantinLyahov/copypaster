d45cb497c594dee488c96c465d159e7037332e6d
Constantinely <43109874+Constantinely@users.noreply.github.com>
2020-11-30 01:50:58 +0200

    removed email verification

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes(['verify' => true]);
-
 Route::get('/', [CopypastaController::class, 'getIndexPage'])->name('index');
-Route::get('/home',  [CopypastaController::class, 'getHomePage'])->middleware(['auth', 'verified'])->name('home');
+Route::get('/home',  [CopypastaController::class, 'getHomePage'])->middleware(['auth'])->name('home');
 Route::get('/create', [CopypastaController::class, 'getCreatePage'])->name('create');
-Route::get('/mypastes', [CopypastaController::class, 'getUserPastes'])->middleware(['auth', 'verified'])->name('user.pastes');
-Route::get('/changepaste/{code}', [CopypastaController::class, 'getPasteChange'])->middleware(['auth', 'verified'])->name('paste.change');
+Route::get('/mypastes', [CopypastaController::class, 'getUserPastes'])->middleware(['auth'])->name('user.pastes');
+Route::get('/changepaste/{code}', [CopypastaController::class, 'getPasteChange'])->middleware(['auth'])->name('paste.change');
 Route::get('/paste/{code}', [CopypastaController::class, 'getPasteRedirect'])->name('get.paste');
-Route::get('/deletepaste/{code}', [CopypastaController::class, 'deletePaste'])->middleware(['auth', 'verified'])->name('paste.delete');
-Route::get('/trash', [CopypastaController::class, 'getTrash'])->middleware(['auth', 'verified'])->name('trash');
-Route::get('/forcedelete/{code}', [CopypastaController::class, 'forceDelete'])->middleware(['auth', 'verified'])->name('force-delete');
-Route::get('/restore/{code}', [CopypastaController::class, 'restore'])->middleware(['auth', 'verified'])->name('restore');
+Route::get('/deletepaste/{code}', [CopypastaController::class, 'deletePaste'])->middleware(['auth'])->name('paste.delete');
+Route::get('/trash', [CopypastaController::class, 'getTrash'])->middleware(['auth'])->name('trash');
+Route::get('/forcedelete/{code}', [CopypastaController::class, 'forceDelete'])->middleware(['auth'])->name('force-delete');
+Route::get('/restore/{code}', [CopypastaController::class, 'restore'])->middleware(['auth'])->name('restore');
 
 Route::post('/paste/{code}', [CopypastaController::class, 'getPaste'])->name('post.paste');
-Route::post('/changepaste/{code}', [CopypastaController::class, 'postPasteChange'])->middleware(['auth', 'verified'])->name('post.paste.change');
+Route::post('/changepaste/{code}', [CopypastaController::class, 'postPasteChange'])->middleware(['auth'])->name('post.paste.change');
 Route::post('/create', [CopypastaController::class, 'postCreate'])->name('post.create');
